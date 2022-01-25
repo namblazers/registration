98d035e0c6d6fceb63fe5ee7894b3f3ff1ae7f83
24chris <40150356+24chris@users.noreply.github.com>
2022-01-25 16:09:37 +0300

    navigation

--- pages/index.js
+++ pages/index.js
@@ -19,7 +19,7 @@ export default function Home() {
     e.preventDefault()
     try{
       const body = {username,contact,address,recommendation}
-      await fetch('http://localhost:3000/api/register',{
+      await fetch('https://namuwongo-basketball-registration-pt20b574y-namblazers.vercel.app/api/register',{
         method:'POST',
         headers:{'Content-Type':'application/json'},
         body:JSON.stringify(body)
